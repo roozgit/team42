d30dcf58ff3164e23c8ca2c646727edc2f4b982a
roozgit <roozbeh.khd@gmail.com>
2016-01-24 07:10:40 -0700

    version 0.0.1 SNAPSHOT

---
+++ pdraw/routes/flying.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* POST home page. */
+router.post('/', function(req, res)  {
+            var query = req.body.query;
+            if(query=="sine") {
+                
+               // var refreshIntervalId=null;
+                var t=0;
+                var arDrone = require('ar-drone');
+                var control = arDrone.createUdpControl();
+                var ref  = {};
+                var pcmd = {};
+            
+                ref.emergency = true;
+                ref.fly       = true;
+            
+            setTimeout(function() {
+                    console.log('Recovering from emergency mode if there was one ...');
+                    ref.emergency=false;
+                    }, 1000);
+            
+            setInterval(function() {
+                    console.log('Moving sinusoidally');
+                    control.ref(ref);
+                    
+                    t+=5;
+                    pcmd.front = .025*t;
+                    if(Math.sin(t)>=0) {    
+                        pcmd.up=5*Math.sin(t/5);
+                        pcmd.down=0;
+                    } else {
+                        pcmd.down=5*Math.sin(t/5);
+                        pcmd.up=0;
+                    }
+                    
+                    control.pcmd(pcmd);
+                    control.flush();
+                }, 500);
+            
+            setTimeout(function() {
+                ref.fly = false;
+                pcmd = {};
+                console.log('Landing ...');
+                }, 16000);
+                
+                 
+            }
+});
+                      
+module.exports = router;
